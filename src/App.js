17499c64e58c800245a2d145853f7c7ce70f25bb
UcheSylvester <okorocode@gmail.com>
2020-05-04 20:52:15 +0100

    moving the contact array into the app component state

--- src/App.js
+++ src/App.js
@@ -2,33 +2,38 @@ import React from "react";
 import "./App.css";
 import ListContacts from "./ListContacts";
 
-const contacts = [
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "@tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "@karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "@richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-];
+class App extends React.Component {
+  state = {
+    contacts: [
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "@tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "@karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "@richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+    ],
+  };
 
-function App() {
-  return (
-    <div>
-      <ListContacts contacts={contacts} />
-    </div>
-  );
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        <ListContacts contacts={contacts} />
+      </div>
+    );
+  }
 }
 
 export default App;
