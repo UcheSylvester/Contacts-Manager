452d59ce37643ff79fe02e10b8270ea3cc2ee641
UcheSylvester <okorocode@gmail.com>
2020-05-04 20:45:22 +0100

    fixed styling for contact-remove button, converted list contaacts to a functional stateless component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListContacts extends Component {
-  render() {
-    // console.log({ props: this.props });
-    const { contacts } = this.props;
-    return (
-      <ol className="contact-list">
-        {contacts.map(({ name, avatarURL, handle }) => (
-          <li key={name} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${avatarURL})`,
-              }}
-            ></div>
+const ListContacts = ({ contacts }) => (
+  <ol className="contact-list">
+    {contacts.map(({ name, avatarURL, handle }) => (
+      <li key={name} className="contact-list-item">
+        <div
+          className="contact-avatar"
+          style={{
+            backgroundImage: `url(${avatarURL})`,
+          }}
+        ></div>
 
-            <div className="contact-name">
-              <p>{name}</p>
-              <p>{handle}</p>
-            </div>
+        <div className="contact-name">
+          <p>{name}</p>
+          <p>{handle}</p>
+        </div>
 
-            <button className="contact-remove">Remove</button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+        <button className="contact-remove">Remove</button>
+      </li>
+    ))}
+  </ol>
+);
 
 export default ListContacts;
