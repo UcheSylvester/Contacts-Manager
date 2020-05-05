3003d5801b44d9a62ac66b298b02509a8a3add81
UcheSylvester <okorocode@gmail.com>
2020-05-05 01:02:58 +0100

    added an imageInput component for image uploads

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./App.css";
 import ListContacts from "./ListContacts";
 
+import { Route } from "react-router-dom";
+
 import * as ContactsAPI from "./utils/ContactsAPI";
+import CreateContacts from "./CreateContacts";
 
 class App extends React.Component {
   state = {
@@ -46,10 +49,18 @@ class App extends React.Component {
     const { contacts } = this.state;
     return (
       <div>
-        <ListContacts
-          contacts={contacts}
-          onDeleteContact={this.removeContact}
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <ListContacts
+              contacts={contacts}
+              onDeleteContact={this.removeContact}
+            />
+          )}
         />
+
+        <Route path="/create" component={CreateContacts} />
       </div>
     );
   }
